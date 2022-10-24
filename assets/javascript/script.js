4268691147dfaa1ab3f684fe7f398871b7158103
Alex Clayton <alexander.clayton.email@gmail.com>
2022-10-24 11:42:13 -0500

    fixed some bugs in the javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,24 +4,19 @@ var cityInput = document.getElementById("input");
 
 var recentSearch = document.getElementById("recent-searches");
 
-//var weatherSection = document.getElementsByClassName("hidden");
-
 var cityName = document.getElementById("city-name");
 var cityTemp = document.getElementById("city-temp");
 var cityWind = document.getElementById("city-wind");
 var cityHumidity = document.getElementById("city-humidity");
+var cityWeatherPic = document.getElementById("city-weather-image");
+var cityWeatherDescription = document.getElementById("city-weather-description");
 
 var fiveDayPull = document.getElementById("5-day-pull");
 
-//var cityLattitude = value;
-
-//var cityLongitude = value1;
-
-var cityCoordinatesURL = ('http://api.openweathermap.org/geo/1.0/direct?q=' + cityInput.value + '&limit=5&appid=aa5a9bd886dab3a65130eaa89476e34e')
-
 function testFunction() {
-
-
+    var cityCoordinatesURL = ('http://api.openweathermap.org/geo/1.0/direct?q=' + cityInput.value + '&limit=5&appid=aa5a9bd886dab3a65130eaa89476e34e')
+   
+   console.log(cityCoordinatesURL)
     fetch(cityCoordinatesURL)
         .then(function (response) {
             return response.json();
@@ -31,6 +26,7 @@ function testFunction() {
             var cityLongitude = data[0].lon;
             var searchCityName = data[0].name + ", " + data[0].country;
             cityName.innerHTML = searchCityName;
+            
            
             
             function cityWeather() {
@@ -42,6 +38,9 @@ function testFunction() {
                     .then(function (data) {
                         var weatherSection = document.getElementById("weather-section");
                         weatherSection.classList.remove("hidden");
+                        var cityWeatherImage = data.weather[0].icon
+                        cityWeatherPic.setAttribute("src","https://openweathermap.org/img/wn/" + cityWeatherImage + "@2x.png");
+                        cityWeatherDescription.innerHTML = data.weather[0].description
                         cityTemp.innerHTML = "Temp: " + (((data.main.temp) - 273.15) * 9/5 + 32).toFixed(1) + "°F";
                         cityWind.innerHTML = "Wind: " + data.wind.speed + "MPH";
                         cityHumidity.innerHTML = "Humidity: " + data.main.humidity + "%";
@@ -55,50 +54,66 @@ function testFunction() {
                     return response.json();
                 })
                 .then(function (data) {
+                    console.log(data)
                     var dayOneDate = document.getElementById("day-one-date");
-                    dayOneDate.innerHTML = data.list[2].dt_txt;
+                    dayOneDate.innerHTML = data.list[6].dt_txt;
+                    var dayOneImg = document.getElementById("day-one-image");
+                    var dayOneWeatherImage = data.list[6].weather[0].icon;
+                    dayOneImg.setAttribute("src", "https://openweathermap.org/img/wn/" + dayOneWeatherImage + "@2x.png");
                     var dayOneTemp = document.getElementById("day-one-temp");
-                    dayOneTemp.innerHTML = "Temp: " + (((data.list[2].main.temp - 273.15) * 9/5 + 32).toFixed(1) + "°F");
+                    dayOneTemp.innerHTML = "Temp: " + (((data.list[6].main.temp - 273.15) * 9/5 + 32).toFixed(1) + "°F");
                     var dayOneWind = document.getElementById("day-one-wind");
-                    dayOneWind.innerHTML = "Wind: " + data.list[2].wind.speed + "MPH";
+                    dayOneWind.innerHTML = "Wind: " + data.list[6].wind.speed + "MPH";
                     var dayOneHumidity = document.getElementById("day-one-humidity");
-                    dayOneHumidity.innerHTML = "Humidity: " + data.list[2].main.humidity + "%";
+                    dayOneHumidity.innerHTML = "Humidity: " + data.list[6].main.humidity + "%";
 
                     var dayTwoDate = document.getElementById("day-two-date");
-                    dayTwoDate.innerHTML = data.list[10].dt_txt;
+                    dayTwoDate.innerHTML = data.list[14].dt_txt;
+                    var dayTwoImg = document.getElementById("day-two-image");
+                    var dayTwoWeatherImage = data.list[14].weather[0].icon;
+                    dayTwoImg.setAttribute("src", "https://openweathermap.org/img/wn/" + dayTwoWeatherImage + "@2x.png");
                     var dayTwoTemp = document.getElementById("day-two-temp");
-                    dayTwoTemp.innerHTML = "Temp: " + (((data.list[10].main.temp - 273.15) * 9/5 + 32).toFixed(1) + "°F");
+                    dayTwoTemp.innerHTML = "Temp: " + (((data.list[14].main.temp - 273.15) * 9/5 + 32).toFixed(1) + "°F");
                     var dayTwoWind = document.getElementById("day-two-wind");
-                    dayTwoWind.innerHTML = "Wind: " + data.list[10].wind.speed + "MPH";
+                    dayTwoWind.innerHTML = "Wind: " + data.list[14].wind.speed + "MPH";
                     var dayTwoHumidity = document.getElementById("day-two-humidity");
-                    dayTwoHumidity.innerHTML = "Humidity: " + data.list[10].main.humidity + "%";
+                    dayTwoHumidity.innerHTML = "Humidity: " + data.list[14].main.humidity + "%";
 
                     var dayThreeDate = document.getElementById("day-three-date");
-                    dayThreeDate.innerHTML = data.list[18].dt_txt;
+                    dayThreeDate.innerHTML = data.list[22].dt_txt;
+                    var dayThreeImg = document.getElementById("day-three-image");
+                    var dayThreeWeatherImage = data.list[22].weather[0].icon;
+                    dayThreeImg.setAttribute("src", "https://openweathermap.org/img/wn/" + dayThreeWeatherImage + "@2x.png");
                     var dayThreeTemp = document.getElementById("day-three-temp");
-                    dayThreeTemp.innerHTML = "Temp: " + (((data.list[18].main.temp - 273.15) * 9/5 + 32).toFixed(1) + "°F");
+                    dayThreeTemp.innerHTML = "Temp: " + (((data.list[22].main.temp - 273.15) * 9/5 + 32).toFixed(1) + "°F");
                     var dayThreeWind = document.getElementById("day-three-wind");
-                    dayThreeWind.innerHTML = "Wind: " + data.list[18].wind.speed + "MPH";
+                    dayThreeWind.innerHTML = "Wind: " + data.list[22].wind.speed + "MPH";
                     var dayThreeHumidity = document.getElementById("day-three-humidity");
-                    dayThreeHumidity.innerHTML = "Humidity: " + data.list[18].main.humidity + "%";
+                    dayThreeHumidity.innerHTML = "Humidity: " + data.list[22].main.humidity + "%";
 
                     var dayFourDate = document.getElementById("day-four-date");
-                    dayFourDate.innerHTML = data.list[26].dt_txt;
+                    dayFourDate.innerHTML = data.list[30].dt_txt;
+                    var dayFourImg = document.getElementById("day-four-image");
+                    var dayFourWeatherImage = data.list[30].weather[0].icon;
+                    dayFourImg.setAttribute("src", "https://openweathermap.org/img/wn/" + dayFourWeatherImage + "@2x.png");
                     var dayFourTemp = document.getElementById("day-four-temp");
-                    dayFourTemp.innerHTML = "Temp: " + (((data.list[26].main.temp - 273.15) * 9/5 + 32).toFixed(1) + "°F");
+                    dayFourTemp.innerHTML = "Temp: " + (((data.list[30].main.temp - 273.15) * 9/5 + 32).toFixed(1) + "°F");
                     var dayFourWind = document.getElementById("day-four-wind");
-                    dayFourWind.innerHTML = "Wind: " + data.list[26].wind.speed + "MPH";
+                    dayFourWind.innerHTML = "Wind: " + data.list[30].wind.speed + "MPH";
                     var dayFourHumidity = document.getElementById("day-four-humidity");
-                    dayFourHumidity.innerHTML = "Humidity: " + data.list[26].main.humidity + "%";
+                    dayFourHumidity.innerHTML = "Humidity: " + data.list[30].main.humidity + "%";
 
                     var dayFiveDate = document.getElementById("day-five-date");
-                    dayFiveDate.innerHTML = data.list[34].dt_txt;
+                    dayFiveDate.innerHTML = data.list[38].dt_txt;
+                    var dayFiveImg = document.getElementById("day-five-image");
+                    var dayFiveWeatherImage = data.list[38].weather[0].icon;
+                    dayFiveImg.setAttribute("src", "https://openweathermap.org/img/wn/" + dayFiveWeatherImage + "@2x.png");
                     var dayFiveTemp = document.getElementById("day-five-temp");
-                    dayFiveTemp.innerHTML = "Temp: " + (((data.list[34].main.temp - 273.15) * 9/5 + 32).toFixed(1) + "°F");
+                    dayFiveTemp.innerHTML = "Temp: " + (((data.list[38].main.temp - 273.15) * 9/5 + 32).toFixed(1) + "°F");
                     var dayFiveWind = document.getElementById("day-five-wind");
-                    dayFiveWind.innerHTML = "Wind: " + data.list[34].wind.speed + "MPH";
+                    dayFiveWind.innerHTML = "Wind: " + data.list[38].wind.speed + "MPH";
                     var dayFiveHumidity = document.getElementById("day-five-humidity");
-                    dayFiveHumidity.innerHTML = "Humidity: " + data.list[34].main.humidity + "%";
+                    dayFiveHumidity.innerHTML = "Humidity: " + data.list[38].main.humidity + "%";
                 })
                 }
 
@@ -107,3 +122,10 @@ function testFunction() {
         })
     
 }
+
+//Search doesn't work on page load
+    //^^FIX: added generated search URL inside the search function
+//cannot search more than 1 city per refresh
+    //^^FIX: added generated search URL inside the search function
+//search seems to run the last value in the input field...
+    //^^FIX: added generated search URL inside the search function
